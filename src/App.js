2375abccd92ce840b46f7a36b7b95795dbd8b6d2
Carlos Morales <carlos.mrojas@alumnos.udg.mx>
2023-09-02 13:23:56 -0600

    Dandole funcionalidad a los componentes y sus elementos, homepage, sign up y login

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/navbar';
 import Landing_Page from './components/Landing_Page/LandingPage';
-
+import SignupForm from './components/Sign-up/sign-up'; 
+import LoginForm from './components/Login/login';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Landing_Page />} />
+          <Route path="/" element={<Landing_Page/>} />
+          <Route path="/signup" element={<SignupForm />} />
+          <Route path="/login" element={<LoginForm/>} />
           {/* Otras rutas de tu aplicación */}
         </Routes>
       </BrowserRouter>
